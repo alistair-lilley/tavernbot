e2cd7484c8be25f62d4f082e31b55442a88b8300
alistair-lilley <jklilley12@gmail.com>
2024-04-21 15:53:49 -0700

    basic cehcks and catching done

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ import asyncio
 import logging
 import json
 
+from typing import Callable
+
 from aiogram import Bot, Router, Dispatcher
 from aiogram import F
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.types import Message
 from aiogram.types.reply_keyboard_markup import ReplyKeyboardMarkup
 from aiogram.types.reply_keyboard_remove import ReplyKeyboardRemove
 from aiogram.types.keyboard_button import KeyboardButton
+from aiogram.types.error_event import ErrorEvent
 
 from dotenv import dotenv_values
 
@@ -36,24 +41,61 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-tgbot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+tgbot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 router = Router()
 dispatcher = Dispatcher()
 dispatcher.include_router(router)
 
 tavernbot = TavernBot(ADMINFILE, POLLSFILE, MEEEEEEEE)
 
-adminids = [int(adminid) for adminid in tavernbot.admin_ids]
+ADMINIDS = [int(adminid) for adminid in tavernbot.admin_ids]
+
+# Command meta-checks
+
+def _check_update_admin(message: Message) -> None:
+    """Decorator over each function to update admin IDs and @s as necessary"""
+    tavernbot.update_admin_info(str(message.from_user.id), message.from_user.username)
+
+def pre_command_check(command: Callable):
+    """Wrap each command with this to do pre-command checks"""
+    async def wrap_command(message: Message):
+        _check_update_admin(message)
+        await command(message)
+    return wrap_command
+
+@router.error(F.update.message.as_("message"))
+async def error_user_didnt_start_bot(event: ErrorEvent, message: Message) -> None:
+    """Tells a user in group to start private messages if they're not started"""
+    LOGGER.critical("User %s (%s) tried to use me in a group before they initiated me privately")
+    await message.reply("Oop you gotta message me directly before you can ask me anything in a group!! Send me <code>/start</code> in private messages please. (If you <em>have</em> started me and something's still going wrong, go poke Ali for debugging!)")
+
+@router.message(
+    (~(F.from_user.id.in_(ADMINIDS)))
+    & ((F.chat.type == "private") | (F.text.startswith("/")))
+)
+async def nonadmin(message: Message):
+    """Validate that a user is an admin if the user is either sending a slash command or talking
+    to the bot in private messages"""
+    LOGGER.info(
+        "User %s (%s) tried to use me, but is not an admin.",
+        message.from_user.username,
+        message.from_user.id,
+    )
+    # Not sure this is even necessary tbh
+    # await message.answer(
+    #     "You are not an admin of this chat. You may not run bot commands."
+    # )
+
+# Commands
 
 @router.message(F.text.startswith("/start"))
-async def start(message):
+@pre_command_check
+async def start(message: Message):
+    """Get a user initiaed with th ebot"""
     LOGGER.info("User %s started with me", message.from_user.username)
-    await message.answer("Hi! Thanks for talking to me! If you're an admin, go ahead and give me commands.")
-
-@router.message((~(F.from_user.id.in_(adminids)))&((F.chat.type == "private")|(F.text.startswith("/"))))
-async def nonadmin(message):
-    LOGGER.info("User %s (%s) tried to use me, but is not an admin.", message.from_user.username, message.from_user.id)
-    await message.answer("You are not an admin of this chat. You may not run bot commands.")
+    await message.answer(
+        "Hi! Thanks for talking to me! If you're an admin, go ahead and give me commands."
+    )
 
 
 if __name__ == "__main__":
--- tavernbot.py
+++ tavernbot.py
@@ -12,6 +12,7 @@ from dataclasses import dataclass
 
 LOGGER = logging.getLogger(__name__)
 
+
 @dataclass
 class PollObject:
     """Object containing information for a poll"""
@@ -32,7 +33,7 @@ class PollObject:
             "replylopts": self.replyopts,
             "schedule": self.schedule,
         }
-    
+
     def deserialize(self: PollObject, fields: Dict):
         """Takes dictionary and constructs object"""
         try:
@@ -41,12 +42,15 @@ class PollObject:
             self.replyopts = fields["replyopts"]
             self.schedule = fields["schedule"]
         except Exception as e:
-            LOGGER.critical(f"Loading poll object failed, likely due to missing field; returned error {e}")
+            LOGGER.critical(
+                f"Loading poll object failed, likely due to missing field; returned error <{e}>"
+            )
+
 
 class TavernBot:
     """Tavern bot
     manages the database parts of the bot
-    the admin dict takes the form of 
+    the admin dict takes the form of
     admins = {
         adminat: adminid,
         ...
@@ -56,7 +60,7 @@ class TavernBot:
     def __init__(self: TavernBot, admin_file: str, polls_file: str, myid: str):
         self._admin_file = admin_file
         self._polls_file = polls_file
-        self._polls: Dict = None 
+        self._polls: Dict = None
         self._admins: Dict = None
         self._default_admins = {"default": myid}
         self._safe_load_objects()
@@ -66,45 +70,53 @@ class TavernBot:
             with open(self._admin_file) as af:
                 self._admins = json.load(af)
         except Exception as e:
-            LOGGER.critical(f"Failed to lkoad admins due to error {e}")
+            LOGGER.critical(f"Failed to load admins due to error <{e}>")
         try:
             with open(self._polls_file) as pf:
                 rawpolls = json.load(pf)
-            self._polls = {poll["name"]: PollObject(polldata) for poll, polldata in rawpolls.items()}
+            self._polls = {
+                poll["name"]: PollObject(polldata)
+                for poll, polldata in rawpolls.items()
+            }
         except Exception as e:
-            LOGGER.critical(f"Failed to load polls due to error {e}")
-        
+            LOGGER.critical(f"Failed to load polls due to error <{e}>")
+
     def _validate_file_existence(self: TavernBot):
         if not os.path.exists(self._admin_file):
             try:
-                with open(self._admin_file, 'w') as af:
+                with open(self._admin_file, "w") as af:
                     json.dump(self._default_admins, af)
             except Exception as e:
-                LOGGER.critical(f"Failed to create admins due to error {e}")
+                LOGGER.critical(f"Failed to create admins due to error <{e}>")
         if not os.path.exists(self._polls_file):
             try:
-                with open(self._polls_file, 'w') as pf:
+                with open(self._polls_file, "w") as pf:
                     json.dump(self._default_admins, pf)
             except Exception as e:
-                LOGGER.critical(f"Failed to create polls due to error {e}")
+                LOGGER.critical(f"Failed to create polls due to error <{e}>")
     
+    def _validate_has_admins(self: TavernBot):
+        if not self._admins:
+            self._admins = self._default_admins.copy()
+
     def _safe_load_objects(self: TavernBot):
         self._validate_file_existence()
         self._load_objects()
-    
+        self._validate_has_admins()
+
     def serialize(self: TavernBot):
         """Dumps admin and polls data to files"""
         try:
-            with open(self._admin_file, 'w') as af:
+            with open(self._admin_file, "w") as af:
                 json.dump(self._admins, af)
         except Exception as e:
-            LOGGER.critical(f"Failed to dump admins due to error {e}")
+            LOGGER.critical(f"Failed to dump admins due to error <{e}>")
         try:
             polldict = {pollname: poll.serialize() for pollname, poll in self._polls}
-            with open(self._polls_file, 'w') as pf:
+            with open(self._polls_file, "w") as pf:
                 json.dump(polldict, pf)
         except Exception as e:
-            LOGGER.critical(f"Failed to dump polls due to error {e}")
+            LOGGER.critical(f"Failed to dump polls due to error <{e}>")
 
     def add_admin(self: TavernBot, adminat: str = None):
         """Add an admin to the dict by @"""
@@ -112,25 +124,36 @@ class TavernBot:
         self.serialize()
 
     def remove_admin(self: TavernBot, admin: str):
+        """Removes an admin from the admin dict by @ *or* ID"""
         if admin in self._admins.keys():
             del self._admins[admin]
         elif admin in self._admins.values():
             # wow this is janky
-            adminkey = list(self._admins.keys())[list(self._admins.values()).index(admin)]
-            del self._admins[adminkey]
+            # basically this finds an admin's @ by searching the values for their id
+            adminat = list(self._admins.keys())[
+                list(self._admins.values()).index(admin)
+            ]
+            del self._admins[adminat]
         self.serialize()
 
     def update_admin_info(self: TavernBot, adminid: str, adminat: str):
         """Update the at:id pairing for admin entries
         Runs each time an admin messages gets caught, just to be sure that the values are up to date
         in case an admin changes their @"""
-        if adminid in self._admins.values() or adminat in self._admins.keys():
+        try:
+            # yeah i know its janky its fine its fine
+            self.remove_admin(adminid)
+            self.remove_admin(adminat)
             self._admins[adminat] = adminid
+            LOGGER.debug("Updated admin %s (%s)", adminat, adminid)
+        except Exception as e:
+            LOGGER.critical("Failed to update admin %s (%s)!", adminat, adminid)
+        self.serialize()
 
     def purge_admins(self: TavernBot):
         self._admins = {}
         self.serialize()
-    
+
     @property
     def admins(self: TavernBot):
         return self._admins
@@ -138,7 +161,7 @@ class TavernBot:
     @property
     def admin_ids(self: TavernBot):
         return self._admins.values()
-    
+
     @property
     def poll_names(self: TavernBot):
         return self._polls.keys()
